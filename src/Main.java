d9d2de8631e58721bb51f7d5cc585c4292cb9625
Jaydesire <elgovsky@ya.ru>
2022-04-05 12:30:36 +0300

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
+
         int c = calc.devide.apply(a, b);
+        // a = 3, b = 0 -- Ошибка деления на 0.
+        // Решена проверкой равенства нулю делителя и знаменателя в лямбда-выражении.
 
         calc.println.accept(c);
     }
